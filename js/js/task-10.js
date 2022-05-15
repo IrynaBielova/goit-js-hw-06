fd2c2d049ee563d9ea5abfc53850343e3c28d401
IrynaBielova <wintergirl@i.ua>
2022-05-15 14:40:11 +0300

    T10-correction

--- js/js/task-10.js
+++ js/js/task-10.js
@@ -1,6 +1,8 @@
+"use strict";
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-};
+}
 
 const inputEl = document.querySelector('input[type="number"]');
 const createEl = document.querySelector("[data-create]");
